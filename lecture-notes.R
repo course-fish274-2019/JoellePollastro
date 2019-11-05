517a71ec90dfddaba2cfbc02bfcb8ff4563a0a60
Joelle Pollastro <joellepollastro@Joelles-MacBook-Air.local>
2019-11-05 10:25:14 -0800

    added comment describing length_cat column

--- lecture-notes.R
+++ lecture-notes.R
@@ -9,7 +9,7 @@ fish_data = read.csv("data/Gaeta_etal_CLC_data.csv")
 
 library(dplyr)
 
-
+# this code adds a column called length_cat, and if length is > 300, big is addded in column
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
