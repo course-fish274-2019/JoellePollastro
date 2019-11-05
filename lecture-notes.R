c12bfa08d5c1918819ca5327f2c73a03483067b7
Joelle Pollastro <joellepollastro@Joelles-MacBook-Air.local>
2019-11-05 10:31:27 -0800

    removed scalelength > 1 and renamed it clean

--- lecture-notes.R
+++ lecture-notes.R
@@ -10,9 +10,12 @@ fish_data = read.csv("data/Gaeta_etal_CLC_data.csv")
 library(dplyr)
 
 # this code adds a column called length_cat, and if length is > 300, big is addded in column
+
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
 
+#Use dplyr to remove the fish with a scalelength of less than 1 mm from fish_data_cat, The new dataset will have 4,029 rows.
 
+fish_data_cat_clean <- filter(fish_data_cat,scalelength >1)
 
